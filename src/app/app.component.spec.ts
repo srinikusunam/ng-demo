f5bc936ffdb75352fb3a5bc4a850db38bb9eefc5
Matt Raible <matt.raible@okta.com>
2020-09-13 03:04:52 -0600

    Update Okta branch to Angular 10 (#81)
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,7 +10,7 @@ describe('AppComponent', () => {
     redirectUri: 'http://localhost:4200'
   };
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -21,7 +21,7 @@ describe('AppComponent', () => {
       ],
       providers: [{provide: OKTA_CONFIG, useValue: oktaConfig}]
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -39,6 +39,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('ng-demo app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-demo!');
   });
 });
